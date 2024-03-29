fed49c7eff7c53e16584f6831d3086971853e0b7
ayoung-iya <rhaehfdl1879@naver.com>
2024-03-29 19:33:38 +0900

    Init: 프로젝트 초기화

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,15 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import { Noto_Sans_KR } from "next/font/google";
+import type { AppProps } from "next/app";
+
+const notoSansKR = Noto_Sans_KR({
+  weight: ["300", "400", "500", "600", "700"],
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <main className={notoSansKR.className}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
