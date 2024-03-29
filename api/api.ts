a24508aea0a7b7bc1e9660585d6986c0350815dc
ayoung-iya <rhaehfdl1879@naver.com>
2024-03-30 01:51:25 +0900

    Feat: 필요한 api, 타입, 상수 추가

---
+++ api/api.ts
@@ -0,0 +1,56 @@
+import { USER_ID, totalFolderId } from '@/util/constants';
+import { Folder, FolderAPI, Link, LinkAPI, User } from '@/types/types';
+
+const BASE_URL = 'https://bootcamp-api.codeit.kr/api';
+
+async function getAPI(query: string) {
+  const response = await fetch(`${BASE_URL}/${query}`);
+
+  if (!response?.ok) {
+    throw new Error('데이터를 불러오는데 실패했습니다.');
+  }
+
+  const body = await response.json();
+
+  return body;
+}
+
+export function getSampleFolder() {
+  return getAPI('sample/folder');
+}
+
+export function getSampleUser() {
+  return getAPI('sample/user');
+}
+
+export async function getUser(): Promise<User> {
+  const response = await getAPI(`users/${USER_ID}`);
+  const data = response.data[0];
+
+  const parsedData = { ...data, imageSource: data['image_source'], authId: data['auth_id'] };
+  return parsedData;
+}
+
+export async function getUserFolders(): Promise<Folder[]> {
+  const response = await getAPI(`users/4/folders`);
+  const parsedData = response.data.map(({ created_at, user_id, ...rest }: FolderAPI) => ({
+    ...rest,
+    createdAt: created_at,
+    userId: user_id,
+  }));
+
+  return parsedData;
+}
+
+export async function getUserLinks(id: number | string): Promise<Link[]> {
+  const query = id === totalFolderId ? `users/${USER_ID}/links` : `users/${USER_ID}/links?folderId=${id}`;
+  const response = await getAPI(query);
+
+  const parsedData = response.data.map(({ created_at, image_source, ...rest }: LinkAPI) => ({
+    ...rest,
+    createdAt: created_at,
+    imageSource: image_source,
+  }));
+
+  return parsedData;
+}
