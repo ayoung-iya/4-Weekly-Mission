d4160a6ccb2423b3921005083d36c3160107465e
ayoung-iya <rhaehfdl1879@naver.com>
2024-03-31 04:10:42 +0900

    Refactor: a태그 Link 컴포넌트로 변경

--- components/layout/footer/IconWithLink.tsx
+++ components/layout/footer/IconWithLink.tsx
@@ -1,5 +1,6 @@
 import { FooterItem } from '@/types/types';
 import styles from './IconWithLink.module.css';
+import Link from 'next/link';
 
 const IconWithLink = ({ name, address, imgUrl }: FooterItem) => {
   const target = /^http/.test(address) ? '_blank' : '_self';
@@ -7,9 +8,9 @@ const IconWithLink = ({ name, address, imgUrl }: FooterItem) => {
 
   return (
     <li>
-      <a href={address} target={target} rel={rel} className={styles.item}>
+      <Link href={address} target={target} rel={rel} className={styles.item}>
         {imgUrl ? <img src={imgUrl} alt={name} /> : `${name}`}
-      </a>
+      </Link>
     </li>
   );
 };
