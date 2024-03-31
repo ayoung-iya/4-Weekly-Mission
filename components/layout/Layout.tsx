b6e61aea97704bec64ec64934c2873cbbe463a6e
ayoung-iya <rhaehfdl1879@naver.com>
2024-03-31 16:07:13 +0900

    Chore: 사용하지 않는 import문 제거

--- components/layout/Layout.tsx
+++ components/layout/Layout.tsx
@@ -1,6 +1,5 @@
 import Header from './header/Header';
 import Footer from './footer/Footer';
-import UserProvider from '@/context/UserContext';
 import { PropsWithChildren } from 'react';
 
 function Layout({ children }: PropsWithChildren) {
