d7ea49c8afc8dafee153dd35fdb73eb137fadd31
ayoung-iya <rhaehfdl1879@naver.com>
2024-04-09 03:34:29 +0900

    Feat: Sign 관련 inputGroup 설정

---
+++ components/pages/sign/InputGroup.tsx
@@ -0,0 +1,38 @@
+import { useFormContext } from 'react-hook-form';
+import styles from '@/styles/sign.module.css';
+import { useState } from 'react';
+
+const InputGroup = ({ info }: any) => {
+  const {
+    register,
+    trigger,
+    formState: { errors },
+  } = useFormContext();
+  const [showText, setShowText] = useState(() => (info.id !== 'email' ? false : true));
+  const type = info.id === 'email' ? info.type : showText ? 'text' : info.type;
+  const eyeClassName = `${styles.iconEye} ${showText ? styles.on : ''}`;
+  const inputClassName = `${styles.input} ${errors[info.id] ? styles.error : ''}`;
+
+  return (
+    <>
+      <label className={styles.label} htmlFor={info.id}>
+        {info.label}
+      </label>
+      <div className={styles.inputGroup}>
+        <input
+          type={type}
+          id={info.id}
+          {...register(info.id, { ...info.validation, onBlur: async () => await trigger(info.id) })}
+          placeholder={info.placeholder}
+          className={inputClassName}
+        />
+        {info.type === 'password' && (
+          <button type="button" className={eyeClassName} onClick={() => setShowText(!showText)} />
+        )}
+      </div>
+      {errors?.[info.id] && <p className={styles.messageError}>{errors[info.id]?.message as string}</p>}
+    </>
+  );
+};
+
+export default InputGroup;
